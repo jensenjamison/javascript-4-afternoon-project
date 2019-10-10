bad79aaea1eb8fd8a5ef4dffdd566e91af065b97
jensenjamison <jensenjamison@yahoo.com>
2019-10-10 18:12:21 -0500

    still working on it

--- classes.js
+++ classes.js
@@ -30,7 +30,17 @@
 */
 
 //Code Here
-
+class Employee {
+  constructor(first_name, last_name, email, age) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+  makeWidget() {
+    return this.first_name + " " + this.last_name + " " + "Widget";
+  }
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -49,7 +59,20 @@
 
 //Code Here
 
-
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age) {
+    super(first_name, last_name, email, age)
+    this.reports = [];
+    
+  }
+  hire(employee) {
+    this.reports.push(employee); 
+  }
+  fire(index) {
+    this.reports.splice(index,index)
+  }
+}
+let carl = new Manager("carl", "grimes", "gmail.com", 15)
 ////////// PROBLEM 3 //////////
 
 /*
@@ -72,7 +95,36 @@
 */
 
 //Code Here
+class ProgressiveManager extends Manager{
+constructor(first_name, last_name, email, age, reports) {
+  super(first_name, last_name, email, age, reports)
+  this.title = "Not a manager";
+  this.bonus = 0;
+  }
 
+  hire(employee) {
+    this.reports.push(employee); 
+
+    if (this.reports.length === 0){
+      this.title = "Not a manager"
+    } else if (this.reports.length >= 1 && this.reports.length <= 3){
+      this.title = "Barely Manager"
+    } else if (this.reports.length >= 4 && this.reports.length <=10){
+      this.title = "Mostly Manager"
+    } else if (this.reports.length >= 11 && this.reports.length <=50){
+      this.title = "Manager" 
+    } else if (this.reports.length >= 51 && this.reports.length <=100){
+      this.title = "Manager Plus" 
+    } else if (this.reports.length >= 101){
+       this.title = "Bestest Manager" 
+    }
+  }
+  fire(index) {
+    this.reports.splice(index,index);
+
+  
+  }
+}
 
 
 ////////// PROBLEM 4 - Black Diamond //////////
